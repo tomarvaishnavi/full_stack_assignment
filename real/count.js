b11697e88a6477bb627988e7424874f614de71c2
tomarvaishnavi <124492421+tomarvaishnavi@users.noreply.github.com>
2024-01-27 00:55:12 +0530

    Add files via upload

--- real/count.js
+++ real/count.js
@@ -29,30 +29,14 @@ $(document).ready(function(){
     // Alternatively, you can trigger the counter animation when the document is ready
     // $(document).ready(function () {
     //     animateCounters();
-    // });
-
-$('.slideInUp').waypoint(function(direction) {
-    //console.log(this.id);
-    myfunc_fadeInDown(this.element, direction); //when using waypoint, this refers to the waypoint object
-},{offset:'90%'});
-
-// $('#fade_in_left').waypoint(function(direction) {
-//     myfunc_fadeInLeft(this.element, direction);
-// },{offset:'10%'});
-
-$('.fadeInLeft').waypoint(function(direction) {
-    myfunc_fadeInLeft(this.element, direction);
-},{offset:'80%'});
-
-$('#slide_in_up').waypoint(function(direction) {
-    myfunc_slideInUp(this.element, direction);
-},{offset:'10%'});
-
-$('.slideInRight').waypoint(function(direction) {
-    myfunc_slideInRight(this.element, direction);
-},{offset:'80%'});
+    // });
 
 
+    $('.slideInUp').waypoint(function(direction) {
+        //console.log(this.id);
+        myfunc_fadeInDown(this.element, direction); //when using waypoint, this refers to the waypoint object
+    },{offset:'90%'});
+    
 function myfunc_fadeInDown(target, direction){
     console.log(target);
     if(direction === "down"){
@@ -65,50 +49,5 @@ function myfunc_fadeInDown(target, direction){
     }
 }
 
-function myfunc_slideInRight(target, direction){
 
-    if(direction === "down"){
-        $(".slideInRight").removeClass("hidden");
-        $(".slideInRight").addClass("animate__animated animate__slideInRight");
-
-        setTimeout(function(){
-            $(".slideInRight").removeClass("animate__animated animate__slideInRight");
-  }, 1000);
-    }
-}
-
-function myfunc_fadeInLeft(target, direction){
-    if(direction === "down"){
-       
-        $('.fadeInLeft').addClass("animate__animated animate__fadeInLeft");
-
-        setTimeout(function(){
-            $('.fadeInLeft').removeClass("animate__animated animate__fadeInLeft");
-            $('.fadeInLeft').removeClass("hidden");
-    }, 1000);
-    }
-}
-
-// function myfunc_slideInUp(target, direction){
-//     if(direction === "down"){
-//         $(target).removeClass("hidden");
-//         $(target).addClass("animated slideInUp");
-//         setTimeout(function(){
-//             $(target).removeClass("animated slideInUp");
-//         }, 1000);
-//     }
-// }
-
-function myfunc_zoomIn(target, direction){
-    if(direction === "down"){
-        $(target).removeClass("hidden");
-        $(target).addClass("animated zoomIn");
-        setTimeout(function(){
-            $(target).removeClass("animated zoomIn");
-    }, 1000);
-    }
-}
-
-
-
- });
+ });
